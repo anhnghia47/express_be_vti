958a5de4431e90897bc79fca5838d3a083744524
steve-nguyen <trongnghiafes@gmail.com>
2024-03-13 22:53:50 +0700

    change field

--- src/services/accountServices.js
+++ src/services/accountServices.js
@@ -57,7 +57,7 @@ const accountService = {
         select 
           AccountID as accountId, Email as email, Username as username, FullName as fullName, 
           A.DepartmentID as departmentId, D.DepartmentName as departmentName, A.PositionID as positionId, 
-          P.PositionName as positionName, CreateDate as createDate, isAdmin 
+          P.PositionName as positionName, CreateDate as createDate, isAdmin, PathImage as avatar
         from Account as A
         left join Department as D on D.DepartmentID = A.DepartmentID
         left join Position as P on P.PositionID = A.PositionID
